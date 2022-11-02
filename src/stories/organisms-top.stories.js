9ef27bb54b76e13be129a9ebb34ebaaf7b479139
hiroyashiratori <105481118+hiroyashiratori@users.noreply.github.com>
2022-11-02 20:43:27 +0900

    Delete organisms-top.stories.js

--- src/stories/organisms-top.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Top from "../components/organisms/top/top.html";
-import "../components/organisms/top/top.scss";
-
-export default { title: "Organisms/Top" };
-
-export const Top = () => Item_Top;
